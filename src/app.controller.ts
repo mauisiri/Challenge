8f5908b241c4981d00a4cecb84bcd6949407c3c4
mauisiri <irisleo.com@gmail.com>
2023-06-27 13:05:32 +0200

    Before Albert Hdez tutorial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,9 @@ import { IsBoolean, IsNotEmpty } from 'class-validator';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { User, CurrentUser } from './user.entity';
 
-class UserPreferencesDto {
+class UserPreferencesDto { //Data that the user can save (DTO)
   @IsBoolean()
   @IsNotEmpty()
   termsAndConditionAccepted: boolean;
@@ -24,28 +24,28 @@ class UserPreferencesDto {
 }
 
 @ApiBearerAuth()
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(AuthGuard('jwt')) //authentication using a JWT
 @ApiTags('user')
 @Controller('v1/user/preferences')
 export class UserPreferencesController {
   constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
+    @InjectRepository(User) //@InjectRepository decorator (to interact with the users table in our database)
+    private userRepository: Repository<User>, //object 
   ) {}
 
   @Post()
-  async saveUserPreferences(
-    @Body() userPreferencesDto: UserPreferencesDto,
-    @CurrentUser() user: User,
+  async saveUserPreferences( //method
+    @Body() userPreferencesDto: UserPreferencesDto, //object
+    @CurrentUser() user: User, // @CurrentUse: custom decorator
   ): Promise<void> {
     const existingUser = await this.userRepository.findOne(user.id);
-    if (existingUser.preferences) {
-      return;
+    if (existingUser.preferences) { //checking if the user has already saved their preferences
+      return; //if he did, return the DTO data.
     }
 
-    const newUser = Object.assign(existingUser, {
+    const newUser = Object.assign(existingUser, { 
       preferences: userPreferencesDto,
-    });
+    }); //if NOT, UPDATE the user's preferences field with the data from the DTO and save the updated user to the database.
 
     await this.userRepository.save(newUser);
   }
